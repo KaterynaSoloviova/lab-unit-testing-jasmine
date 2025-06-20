fe04028932a6c4ad44da18cf152073339eedf072
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-06-20 20:11:29 +0200

    Solved lab with unit testing Jasmine

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,12 @@
-function divide () {
-       
+function divide (num1, num2) {
+    
+    if(num2 == 0){
+        return undefined;
+    }
+
+    if (typeof num1 === "number" && typeof num2 === "number" ){
+        return num1 / num2;  
+    }
+    return undefined;
+     
 }
